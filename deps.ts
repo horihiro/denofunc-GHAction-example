a31c964853c94075487e1370ac446d9c9ae552f7
Hirofumi Horikawa <hori.kawa.shima.tool+github@gmail.com>
2021-11-23 14:14:03 +0900

    Initial commit

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { AzureFunctionsWorker } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/main/mod.ts"
+export type { Context as AzureFunctionsContext } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/main/types.ts";
